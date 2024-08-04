83fa8bf2875b51d71ffac147029b46566355b5a1
silvina@pixeldigita.com <silvina@pixeldigita.com>
2024-08-04 14:04:39 -0300

    added firts structure for the final proyect

---
+++ App.js
@@ -0,0 +1,56 @@
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  Text,
+} from "react-native";
+import Home from "./src/Screens/Home"
+import { colors } from "./src/global/colors";
+// SplashScreen.preventAutoHideAsync();
+
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraLighItalic": require("./assets/fonts/Poppins-ExtraLightItalic.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Barlow-Bold": require("./assets/fonts/Barlow-Bold.ttf"),
+    "Barlow-ExtraLighItalic": require("./assets/fonts/Barlow-ExtraLightItalic.ttf"),
+    "Barlow-Italic": require("./assets/fonts/Barlow-Italic.ttf"),
+    "Barlow-LighItalic": require("./assets/fonts/Barlow-LightItalic.ttf"),
+    "Barlow-Regular": require("./assets/fonts/Barlow-Regular.ttf"),
+    "Barlow-SemiBold": require("./assets/fonts/Barlow-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <SafeAreaView style={styles.container}>
+       <Home />
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    backgroundColor: colors.lightGray,
+  },
+});
---
+++ src/global/colors.js
@@ -0,0 +1,17 @@
+export const colors = {
+    lightGray: "#EDF1D6",
+    grayNatural: '#F6F9F8',
+    green100: "#E1F6D1",
+    green200:'#CBEAB3',
+    green700: "#609966",
+    green900: "#0D3F35",
+    gray100: "#E0E1DD",
+    black: "#000003",
+    white: "#ffffff",
+    orange: "#F18F26",
+    purple: "#EAE2F4",
+    lightOrange: "#F5E7E0",
+    lightOrange2: "#ECE1C8",
+    lightGreen: "#E6F1E3",
+    darkOrange: "#E7632A"
+  };
